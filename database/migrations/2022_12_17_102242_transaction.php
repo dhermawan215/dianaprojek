e9056101967d208dedfb45f81feedf0e1806a6a3
dhermawan215 <dhermawan215@gmail.com>
2022-12-18 13:17:00 +0700

    first commit

---
+++ database/migrations/2022_12_17_102242_transaction.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Transaction extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->integer('qty');
+            $table->integer('totals');
+            $table->date('date_transaction');
+            $table->string('receipt', 2048)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
